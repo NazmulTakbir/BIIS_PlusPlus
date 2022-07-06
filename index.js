f460ff49713ccb3d69fad403f7393be51d4650d3
NazmulTakbir <nazmultakbir98@gmail.com>
2022-07-06 18:54:19 +0600

    correction to baseline 1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require("path");
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use("/", express.static(path.join(__dirname, "client/build")));
+
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static(path.join(__dirname, "client/build")));
 }
