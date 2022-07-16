4098f1d3ccf3c18fe47c540c5bfbbf81cf5f6571
AfnanCSE98 <sihatafnan15.9.1997@gmail.com>
2022-07-16 20:49:05 +0600

    scholarship(except form download),generalinfo,duess logics added

--- controllers/Student/DuesController.js
+++ controllers/Student/DuesController.js
@@ -4,16 +4,58 @@ const session_id = require("../../placeHolder");
 
 const getPendingDues = async (req, res, next) => {
   try {
-    res.status(201).json({ message: "getPendingDues" });
+    const sid = req.params.sid;
+    const stat = 'Not Paid';
+    let queryRes = await pool.query(
+      'select dt.description , dt.amount , d.deadline , d.payment_date , d.dues_status \
+      from dues as d , "dues type" as dt \
+      where d.dues_type_id = dt."dues type id" and d.dues_status = $2 and d.student_id = $1  ' , [sid , stat]
+    );
+
+    pending_dues_list = []
+    
+    for(const element of queryRes.rows){
+      due_obj = {};
+      due_obj["description"] = element["description"];
+      due_obj["amount"]  = element["amount"];
+      due_obj["deadline"] = element["deadline"];
+      due_obj["payment_date"] = element["payment_date"]
+      due_obj["dues_status"] = element["dues_status"];
+
+      pending_dues_list.push(due_obj);
+    }
+
+    res.status(201).json({ message: "getPendingDues"  , pending_dues_list:pending_dues_list});
+
   } catch (err) {
-    const error = new HttpError("Fetching Courses to Add Failed", 500);
+    const error = new HttpError("Fetching pending dues Failed", 500);
     return next(error);
   }
 };
 
 const getPaidDues = async (req, res, next) => {
   try {
-    res.status(201).json({ message: "getPaidDues" });
+    const sid = req.params.sid;
+    const stat = 'Paid';
+    let queryRes = await pool.query(
+      'select dt.description , dt.amount , d.deadline , d.payment_date , d.dues_status \
+      from dues as d , "dues type" as dt \
+      where d.dues_type_id = dt."dues type id" and d.dues_status = $2 and d.student_id = $1  ' , [sid , stat]
+    );
+
+    dues_list = []
+    
+    for(const element of queryRes.rows){
+      due_obj = {};
+      due_obj["description"] = element["description"];
+      due_obj["amount"]  = element["amount"];
+      due_obj["deadline"] = element["deadline"];
+      due_obj["payment_date"] = element["payment_date"];
+      due_obj["dues_status"] = element["dues_status"];
+
+      dues_list.push(due_obj);
+    }
+    res.status(201).json({ message: "getPaidDues" , dues_list:dues_list});
   } catch (err) {
     const error = new HttpError("Fetching Courses to Drop Failed", 500);
     return next(error);
--- controllers/Student/GeneralInfoController.js
+++ controllers/Student/GeneralInfoController.js
@@ -1,10 +1,29 @@
+const { deepOrange } = require("@mui/material/colors");
 const pool = require("../../db");
 const HttpError = require("../../models/HttpError");
 const session_id = require("../../placeHolder");
 
 const getAcademicCalender = async (req, res, next) => {
   try {
-    res.status(201).json({ message: "getAcademicCalender" });
+    let queryRes = await pool.query(
+      'select * from session_phase where session_id=$1' , [session_id]
+    );
+    
+    phase_list = [];
+
+    for(const element of queryRes.rows){
+      var phase_obj = {};
+      phase_obj["phase_number"] = element["phase_number"];
+      phase_obj["description"] = element["description"];
+      phase_obj["start_date"] = element["start_date"];
+      phase_obj["end_date"] = element["end_date"];
+      phase_obj["no_of_weeks"] = element["no_of_weeks"];
+      
+      phase_list.push(phase_obj);
+    }
+
+    res.status(201).json({ message: "getAcademicCalender"  ,  phase_list : phase_list});
+
   } catch (err) {
     const error = new HttpError("Fetching Courses to Add Failed", 500);
     return next(error);
@@ -13,7 +32,22 @@ const getAcademicCalender = async (req, res, next) => {
 
 const getHallInfo = async (req, res, next) => {
   try {
-    res.status(201).json({ message: "getHallInfo" });
+    const sid = req.params.sid;
+    let queryRes = await pool.query(
+      'select h.hall_id , h.hall_name , h.supervisor_name , h.supervisor_phone , h.supervisor_email \
+      from hall as h , student as s \
+      where s.hall_id = h.hall_id and s.student_id = $1' , [sid]
+    );
+    var _row_ = queryRes.rows[0];
+    var hall_obj = {
+      hall_id : _row_["hall_id"],
+      hall_name : _row_["hall_name"],
+      supervisor_name : _row_["supervisor_name"],
+      supervisor_phone : _row_["supervisor_phone"],
+      supervisor_email : _row_["supervisor_email"]
+    }
+
+    res.status(201).json({ message: "getHallInfo" , hall_obj : hall_obj});
   } catch (err) {
     const error = new HttpError("Fetching Courses to Drop Failed", 500);
     return next(error);
@@ -22,7 +56,23 @@ const getHallInfo = async (req, res, next) => {
 
 const getNotices = async (req, res, next) => {
   try {
-    res.status(201).json({ message: "getNotices" });
+    let queryRes = await pool.query(
+      'select * from notice'
+    );
+    
+    notice_list = []
+
+    for(const element of queryRes.rows){
+      notice_obj = {};
+      notice_obj["notice_id"] = element["notice_id"];
+      notice_obj["description"] = element["description"];
+      notice_obj["file_path"] = element["file_path"];
+      notice_obj["upload_date"] = element["upload_date"];
+
+      notice_list.push(notice_obj);
+    }
+
+    res.status(201).json({ message: "getNotices" ,  notice_list:notice_list});
   } catch (err) {
     const error = new HttpError("Fetching Courses to Drop Failed", 500);
     return next(error);
