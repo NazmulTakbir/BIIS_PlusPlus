02bcace2c209a5356d080a3837917000feeabe13
AfnanCSE98 <sihatafnan15.9.1997@gmail.com>
2022-07-17 19:47:43 +0600

    hall-name , dept_name, DoB done

--- controllers/Student/InfoController.js
+++ controllers/Student/InfoController.js
@@ -2,11 +2,35 @@ const pool = require("../../db");
 const HttpError = require("../../models/HttpError");
 const session_id = require("../../placeHolder");
 
+//util
+const get_dept_hall = async (sid) => {
+  let queryRes = await pool.query(
+    'SELECT d.dept_name , h.hall_name from student as s,department as d,hall as h \
+     where s.student_id = $1 and d.dept_id = s.dept_id and h.hall_id = s.hall_id \
+     ',
+    [sid]
+  );
+
+  var returnedObject = {};
+  returnedObject["dept_name"] = queryRes.rows[0]["dept_name"];
+  returnedObject["hall_name"] = queryRes.rows[0]["hall_name"];
+  //console.log(returnedObject);
+  return returnedObject;
+}
+
 const getHomeInfo = async (req, res, next) => {
   try {
+    const sid = req.params.sid;
+    const dept_name  = (await get_dept_level_term(sid)).dept_name;
+    const hall_name = (await get_dept_level_term(sid)).hall_name;
+
+    console.log(hall_name);
+    
     let queryRes = await pool.query("SELECT * from student where student_id = $1", [req.params.sid]);
     const studentInfo = queryRes.rows[0];
-    res.json(studentInfo);
+    const dob = queryRes.rows[0]["date_of_birth"].substring(0,10);
+
+    res.json(studentInfo , {hall_name:hall_name} , {dept_name:dept_name} , {date_of_birth:dob});
   } catch (err) {
     const error = new HttpError("Fetching Student Info Failed", 500);
     return next(error);
