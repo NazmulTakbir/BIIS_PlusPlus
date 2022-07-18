de25c349ba31bdc8bdcb6e86f5fe367d603c20bc
NazmulTakbir <nazmultakbir98@gmail.com>
2022-07-18 11:00:08 +0600

    new feedback page minor change

--- client/src/student/pages/FeedbackComplaint/FeedbackComplaintNew.js
+++ client/src/student/pages/FeedbackComplaint/FeedbackComplaintNew.js
@@ -10,9 +10,9 @@ import { NavbarData } from "./NavbarData";
 import "../../../shared/components/MainContainer.css";
 import Textbox from "../../../shared/components/Textbox/Textbox";
 import RadioButton from "../../../shared/components/RadioButton/RadioButton";
-import CheckboxSingle from "../../../shared/components/CheckboxSingle/CheckboxSingle"
+import CheckboxSingle from "../../../shared/components/CheckboxSingle/CheckboxSingle";
 import CustomButton from "../../../shared/components/CustomButton/CustomButton";
-import Stack from '@mui/material/Stack';
+import Stack from "@mui/material/Stack";
 
 // const studentID = require("../../../placeHolder");
 
@@ -28,75 +28,53 @@ const FeedbackComplaintNew = () => {
           <div className="main_container">
             <div className="content">
               <Navbar NavbarData={NavbarData} />
-              
-          {/* WRAP AROUND REACT FORM     */}
 
-              <Textbox 
-                height="40px" 
+              {/* WRAP AROUND REACT FORM     */}
+
+              <Textbox
+                height="40px"
                 width="450px"
                 resize="none"
-                name="subject" 
+                name="subject"
                 placeholder="Type the subject here"
-                label="Subject"/>
+                label="Subject"
+              />
 
-              <Textbox 
+              <Textbox
                 width="450px"
-                height="150px" 
+                height="150px"
                 resize="vertical"
-                name="feedback" 
+                name="feedback"
                 placeholder="Type your feedback here"
-                label="Subject"/>   
+                label="Subject"
+              />
 
-              <RadioButton 
-                label="Receiver"
-                name="receiver"
-                options={['Web Admin', 'Advisor', 'Department Head']}
-                />   
+              <RadioButton label="Receiver" name="receiver" options={["Advisor", "Department Head"]} />
 
-              <CheckboxSingle 
-                width="450px"
-                name="email_option"
-                label="Receive Email Upon Reply"/>
-
-              <div 
+              <div
                 className="buttons-stack"
                 style={{
-                  margin: "auto"
-                }}>
-                  <Stack 
-                    spacing={2} 
-                    direction="row"
-                    style={{
-                      margin: "auto",
-                      width: "350px",
-                      padding: "10px",
-                      textAlign: "center",
-                      justifyContent: "space-between",
-                    }}>
-                      
-                          <CustomButton 
-                            label="Clear All"
-                            variant="outlined"
-                            color="#b13137"
-                            bcolor="#ffffff"
-                          />
+                  margin: "auto",
+                }}
+              >
+                <Stack
+                  spacing={2}
+                  direction="row"
+                  style={{
+                    margin: "auto",
+                    width: "350px",
+                    padding: "10px",
+                    textAlign: "center",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <CustomButton label="Clear All" variant="outlined" color="#b13137" bcolor="#ffffff" />
 
-                          <CustomButton 
-                            label="Submit"
-                            variant="contained"
-                            color="#ffffff"
-                            bcolor="#b13137"
-                          /> 
-                  </Stack>
+                  <CustomButton label="Submit" variant="contained" color="#ffffff" bcolor="#b13137" />
+                </Stack>
               </div>
 
-          {/* WRAP AROUND REACT FORM     */}
-
-
-
-
-
-
+              {/* WRAP AROUND REACT FORM     */}
             </div>
           </div>
         </div>
