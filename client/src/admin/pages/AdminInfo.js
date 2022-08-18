dc647a6acc39b6237ab30bfecd6061380ab73aaa
NazmulTakbir <nazmultakbir98@gmail.com>
2022-08-18 13:33:54 +0600

    admin info code refactored

--- client/src/admin/pages/AdminInfo.js
+++ client/src/admin/pages/AdminInfo.js
@@ -4,14 +4,13 @@ import Sidebar from "../../shared/components/Sidebar/Sidebar";
 import Header from "../../shared/components/Header/Header";
 import { SidebarData } from "../components/SidebarData";
 
-import ComptrollerAdminProfile from "../../shared/components/AdminProfile/ComptrollerAdminProfile";
-import OfficeAdminProfile from "../../shared/components/AdminProfile/OfficeAdminProfile";
-import HallAdminProfile from "../../shared/components/AdminProfile/HallAdminProfile";
-import DepartmentAdminProfile from "../../shared/components/AdminProfile/DepartmentAdminProfile"
+import ComptrollerAdminProfile from "./AdminProfile/ComptrollerAdminProfile";
+import OfficeAdminProfile from "./AdminProfile/OfficeAdminProfile";
+import HallAdminProfile from "./AdminProfile/HallAdminProfile";
+import DepartmentAdminProfile from "./AdminProfile/DepartmentAdminProfile";
 
 import { AuthContext } from "../../shared/context/AuthContext";
 import "../../shared/components/MainContainer.css";
-
 
 const AdminInfo = () => {
   const auth = useContext(AuthContext);
@@ -19,18 +18,17 @@ const AdminInfo = () => {
   const renderCell = () => {
     switch (auth.userType) {
       case "comptroller admin":
-        return <ComptrollerAdminProfile/>    
+        return <ComptrollerAdminProfile />;
       case "office admin":
-        return <OfficeAdminProfile/>;
+        return <OfficeAdminProfile />;
       case "hall admin":
-        return <HallAdminProfile/>;
+        return <HallAdminProfile />;
       case "department admin":
-        return <DepartmentAdminProfile/>;
+        return <DepartmentAdminProfile />;
       default:
         return null;
     }
   };
-
 
   return (
     <React.Fragment>
@@ -48,8 +46,6 @@ const AdminInfo = () => {
       </div>
     </React.Fragment>
   );
-
-  
 };
 
 export default AdminInfo;
--- client/src/admin/pages/AdminProfile/HallAdminProfile.js
+++ client/src/admin/pages/AdminProfile/HallAdminProfile.js
@@ -3,9 +3,11 @@ import React, { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../../../shared/context/AuthContext";
 import "./AdminProfile.css";
 
-const ComptrollerAdminProfile = () => {
+const HallAdminProfile = (props) => {
   const auth = useContext(AuthContext);
   const [name, setName] = useState("");
+  //usestate for hall_name
+  const [hall_name, setHall_name] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,13 +17,13 @@ const ComptrollerAdminProfile = () => {
         });
         const jsonData = await response.json();
         setName(jsonData.ProfileData.name);
+        setHall_name(jsonData.ProfileData.hall_name);
       } catch (err) {
         console.log(err);
       }
     };
     fetchData();
   }, [auth]);
-
   return (
     <div className="profile-container">
       <div className="profile-card">
@@ -29,13 +31,18 @@ const ComptrollerAdminProfile = () => {
 
         <div className="profile-data">
           <div className="item">
+            <div className="heading">Admin Type </div>
+            <div className="text">{auth.userType}</div>
+            <br />
             <div className="heading">Name: </div>
             <div className="text">{name}</div>
-          </div>      
+            <div className="heading">Hall Name: </div>
+            <div className="text">{hall_name}</div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default ComptrollerAdminProfile;
+export default HallAdminProfile;
